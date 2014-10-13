a9e646417806060f293f02d7686044d54dbf5ec9
justinthrelkeld <justin.threlkeld@gmail.com>
2014-10-13 11:55:24 -0500

    illuminate the keys

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -90,6 +90,12 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
+
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
