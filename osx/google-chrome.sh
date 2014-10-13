271aa0d5bb6672bbaf5f9ec295317ef8da36c645
justinthrelkeld <justin.threlkeld@gmail.com>
2014-10-13 14:41:34 -0500

    start google chrome config script

---
+++ osx/google-chrome.sh
@@ -0,0 +1,3 @@
+# Disable the all too sensitive backswipe
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
